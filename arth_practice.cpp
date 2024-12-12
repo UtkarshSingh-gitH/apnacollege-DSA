f82218b7afe9a7f4e450caaa8f38c16ed1a94731
UtkarshSingh-gitH <shivsae21@gmail.com>
2024-12-12 21:03:39 +0530

    second lecture done and hw file

---
+++ arth_practice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    double result;
+    cout<<"Enter the first value :"<<endl;
+    cin>>a;
+    cout<<"Enter the second value :"<<endl;
+    cin>>b;
+
+    cout<<"The sum of 2 values is :"<<(a+b)<<endl;
+    cout<<"The differnce of 2 value is :"<<(a-b)<<endl;
+    cout<<"The multipliaction of 2 value is :"<<(a*(float)b)<<endl;
+    cout<<"The division of 2 value is :"<<((double)a/b)<<endl;
+    cout<<"thank you!"<<endl;
+
+    return 0;
+}
