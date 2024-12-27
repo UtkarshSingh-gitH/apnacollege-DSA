2b914d13d9cae79604fc094743ab8ff664aa6dba
UtkarshSingh-gitH <shivsae21@gmail.com>
2024-12-27 17:32:38 +0530

    revision for logic

--- patterns/pyramid.cpp
+++ patterns/pyramid.cpp
@@ -6,26 +6,22 @@ int main()
     int n;
     cout<<"Enter the size of pyramid ";
     cin>>n;
-    for(int i=1;i<=n;i++)
+    for(int i=0;i<n;i++)
     {
         int num1=1;
-        for(int j=0;j<n-i;j++)
+        for(int j=n;j>i+1;j--)
         {
             cout<<" ";
         }
-        for(int k=0; k<i;k++)
+        for(int k=0;k<i+1;k++)
         {
             cout<<num1;
             num1++;
         }
-        for(int m=i-1;m>0;m--)
+        for(int l=i;l>0;l--)
         {
-            cout<<m;
+            cout<<l;
         }
-        // for(int l=1;l<n;l++)
-        // {
-        //     cout<<" ";
-        // }
         cout<<endl;
 
     }
